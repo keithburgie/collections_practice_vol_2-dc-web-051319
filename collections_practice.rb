4d37dad93bea1ee6d1eceba02d1d67a2f6f7f8b1
Keith Burgie <krburgie@gmail.com>
2019-05-06 19:16:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -74,11 +74,11 @@ def organize_schools(data)
   end
   
   locations.each do |keys, location_arr|
-      data.each do |school, location| 
-          if location.values.join == keys
-             location_arr << school
-          end
-      end
+    data.each do |school, location| 
+        if location.values.join == keys
+           location_arr << school
+        end
+    end
   end
   locations
 end
